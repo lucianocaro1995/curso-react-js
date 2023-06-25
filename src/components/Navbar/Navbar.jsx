e0d390e86f9c98e439e258121370ec7d9e469dd6
lucianocaro1995 <lucho_lp_1995@hotmail.com>
2023-06-25 06:51:39 -0300

    Navbar arreglado

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,39 +5,39 @@ const Navbar = () => {
     return (
         <ul className="categorias">
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/laptop'}>
-                    <img src="https://i.ibb.co/BqV9VZg/laptop.png" alt="Categoria laptops" />
-                    <span className="categoria-span">Laptops</span>
+                <NavLink className={"categoria-link"} to={'/categoria/Notebook'}>
+                    <img src="https://i.ibb.co/BqV9VZg/laptop.png" alt="Categoria notebooks" />
+                    <span className="categoria-span">Notebooks</span>
                 </NavLink>
             </li>
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/tablet'}>
-                    <img src="https://i.ibb.co/zhq5Rnc/tablet.png" alt="Categoria tablets" />
-                    <span className="categoria-span">Tablets</span>
+                <NavLink className={"categoria-link"} to={'/categoria/Auriculares'}>
+                    <img src="https://i.ibb.co/XSHFmzK/auricularj.png" alt="Categoria auriculares" />
+                    <span className="categoria-span">Auriculares</span>
                 </NavLink>
             </li>
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/tv'}>
-                    <img src="https://i.ibb.co/zN9KwGm/tv.png" alt="Categoria tv" />
-                    <span className="categoria-span">Tv</span>
+                <NavLink className={"categoria-link"} to={'/categoria/Mouse'}>
+                    <img src="https://i.ibb.co/CvGmTqf/af.png" alt="Categoria mouses" />
+                    <span className="categoria-span">Mouses</span>
                 </NavLink>
             </li>
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/monitor'}>
+                <NavLink className={"categoria-link"} to={'/categoria/Monitor'}>
                     <img src="https://i.ibb.co/xXRSRPW/monitor.png" alt="Categoria monitores" />
                     <span className="categoria-span">Monitores</span>
                 </NavLink>
             </li>
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/pc'}>
-                    <img src="https://i.ibb.co/ZWRDqk8/pc.png" alt="Categoria pc" />
-                    <span className="categoria-span">Pc</span>
+                <NavLink className={"categoria-link"} to={'/categoria/Pc'}>
+                    <img src="https://i.ibb.co/ZWRDqk8/pc.png" alt="Categoria pc gamer" />
+                    <span className="categoria-span">Pc gamer</span>
                 </NavLink>
             </li>
             <li className="categorias-li">
-                <NavLink className={"categoria-link"} to={'/categoria/celular'}>
-                    <img src="https://i.ibb.co/VjR2Wj9/smartphone.png" alt="Categoria celulares" />
-                    <span className="categoria-span">Celulares</span>
+                <NavLink className={"categoria-link"} to={'/categoria/Teclado'}>
+                    <img src="https://i.ibb.co/L6FX9br/te.png" alt="Categoria teclado" />
+                    <span className="categoria-span">Teclados</span>
                 </NavLink>
             </li>
         </ul>
